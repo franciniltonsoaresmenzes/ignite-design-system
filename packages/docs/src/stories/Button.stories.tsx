6f0361a850a8c93828f1943773dbe5903a980666
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-02-24 23:22:22 -0300

    feat(componente: button): componente: Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,50 @@
+import { ArrowRight } from 'phosphor-react'
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secundary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secundary',
+    children: 'Create new',
+  },
+}
+
+export const Tertiray: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WitchIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
